30036ec5bd21bd674a9e02abdd5cdf24b784a5c4
Александр Пфаргер <Sasha-861@mail.ru>
2024-11-22 22:52:57 +0300

    lesson_1

--- python.py
+++ python.py
@@ -1,6 +1,23 @@
-from random import randint
+class Employee:
+    vacation_days = 28
 
-def hello():
-    temp = randint(0, 100)
-    print(f'Привет, Мир!, {temp} раз!')
-hello()   
+    def __init__(self, first_name, second_name, gender):
+        self.first_name = first_name
+        self.second_name = second_name
+        self.gender = gender
+        self.remaining_vacation_days = Employee.vacation_days
+
+        # Сюда добавьте новый атрибут remaining_vacation_days
+
+    def consume_vacation(self, number_out):
+        self.remaining_vacation_days -= number_out     
+    # print(number_out)
+    def get_vacation_details(self):
+        return (f'Остаток отпускных дней: {self.remaining_vacation_days}')
+    # Сюда добавьте методы consume_vacation и get_vacation_details.
+
+
+# Пример использования класса:
+employee = Employee(first_name="Иван", second_name="Петров", gender="м")
+employee.consume_vacation(7)
+print(employee.get_vacation_details())
